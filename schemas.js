b3a1e7c7942753903f4b9b503b7fa74afe7bfd9c
Miki Shimura <miki.shimura.ves@gmail.com>
2022-12-22 20:12:10 +0100

    add review schemas

--- schemas.js
+++ schemas.js
@@ -11,4 +11,11 @@ module.exports.placeSchema = Joi.object({
         ages: Joi.array(),
     }).required(),
     // deleteImages: Joi.array()
-})
+});
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().min(1).max(5).required(),
+        body: Joi.string().required()
+    }).required()
+});
