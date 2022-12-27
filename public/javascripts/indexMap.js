0cb7e26d87660e6d40a173f935f3de69b8e48dfb
Miki Shimura <miki.shimura.ves@gmail.com>
2022-12-27 14:47:17 +0100

    add indexMap

---
+++ public/javascripts/indexMap.js
@@ -0,0 +1,35 @@
+mapboxgl.accessToken = mapToken
+
+const map = new mapboxgl.Map({
+    container: 'index-map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [20.460005315344528, 44.816095610744426],
+    zoom: 11,
+    projection: 'globe'
+});
+map.on('style.load', () => {
+    map.setFog({});
+});
+
+
+
+for(let place of places) {
+    let color = null;
+    if (place.category === "Culture") { 
+        color = { "color": "#0d6efd" } 
+    }else if(place.category === "Sport") { 
+        color = { "color": "#198754" }
+    }else if(place.category === "Education") { 
+        color = { "color": "#dc3545" }
+    }else if(place.category === "Nature") { 
+        color = { "color": "#ffc107" }
+    };
+    new mapboxgl.Marker(color)
+    .setLngLat([place.geometry.longtitude, place.geometry.latitude])
+    .setPopup(
+        new mapboxgl
+        .Popup({ offset: 25 })
+        .setHTML(`<a href="/places/${place._id}">${place.title}</a><p>${place.location}</p>`)
+    )
+    .addTo(map);
+}
